fe4ea7c64f067292e6cf2aa583586e7ae4697617
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2020-01-31 17:05:47 +0300

    ui deadlock fix example

--- WPF.Edu/Views/ViewToFreezeApp.xaml.cs
+++ WPF.Edu/Views/ViewToFreezeApp.xaml.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using ReactiveUI;
 
 namespace WPF.Edu.Views
 {
@@ -12,6 +16,13 @@ namespace WPF.Edu.Views
     public partial class ViewToFreezeApp : UserControl
     {
         private object _lock = new object();
+
+        private static readonly Func<IScheduler, Control, IDisposable> _func = (s, c) =>
+        {
+            c.Background = new SolidColorBrush(Colors.Black);
+
+            return Disposable.Empty;
+        };
 
         public ViewToFreezeApp()
         {
@@ -38,7 +49,9 @@ namespace WPF.Edu.Views
             lock (_lock)
             {
                 Thread.Sleep(100);
-                Application.Current.Dispatcher.Invoke(() => Background = new SolidColorBrush(Colors.Black));
+                RxApp.MainThreadScheduler.Schedule(this, (sd, st) => Background = new SolidColorBrush(Colors.Black));
+                //Application.Current.Dispatcher.Invoke(() => Background = new SolidColorBrush(Colors.Black));
+
             }
         }
     }
