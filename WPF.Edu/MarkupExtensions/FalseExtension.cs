da7ae2642e34af6cbc2bd9e5cd56b2223d79fba3
Gotcha7770 <gbakanov@inbox.ru>
2021-05-24 17:24:58 +0300

    Some Markup extensions

---
+++ WPF.Edu/MarkupExtensions/FalseExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPF.Edu.MarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(bool))]
+    public class FalseExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => false;
+    }
+}
---
+++ WPF.Edu/MarkupExtensions/TypeNameExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPF.Edu.MarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(string))]
+    public class TypeNameExtension : MarkupExtension
+    {
+        public Type Type { get; set; }
+
+        public bool FullName { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return FullName ? Type.FullName : Type.Name;
+        }
+    }
+}
